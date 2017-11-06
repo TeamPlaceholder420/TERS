b2eaa10275cc95b3fefc5888c7f5a6ee81286dd0
Omar Alhammadi <sequencek@gmail.com>
2017-11-06 22:04:36 +0400

    added project files

---
+++ src/TrafficSystem/TrafficMap.java
@@ -0,0 +1,23 @@
+package TrafficSystem;
+
+public class TrafficMap {
+	private PathSection[] roadSections;
+	private PathNode[] pathNodes;
+}
+
+
+class Road {
+	
+}
+
+class PathSection {
+	private int id;
+	
+	
+	
+}
+
+class PathNode {
+	private int id;
+	
+}
---
+++ src/UserInterface/MainFrame.java
@@ -0,0 +1,13 @@
+package UserInterface;
+
+import javax.swing.*;
+
+public class MainFrame {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.setVisible(true);
+	}
+	
+}
